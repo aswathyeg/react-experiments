1ed9760012184575252c025bdef93f973703bf86
aswathyeg <aswathy.eg.bijesh@gmail.com>
2020-12-15 01:26:01 +0530

    formatted

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -1,5 +1,6 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import { render } from 'react-dom';
+
 
 interface IProps {
 
@@ -7,41 +8,41 @@ interface IProps {
 interface IState {
     textcontent?: any
 }
-function BoilingVerdict(props:any):JSX.Element{
-    if(props. celcious>=100){
-        return(
+function BoilingVerdict(props: any): JSX.Element {
+    if (props.celcious >= 100) {
+        return (
             <p>The water would boil.</p>)
-        
+
     }
     return <p>The water would not boil.</p>;
 
-   
+
 }
-class Calculator extends React.Component<IProps,IState>{
-    constructor(props:any){
+class Calculator extends React.Component<IProps, IState>{
+    constructor(props: any) {
         super(props);
-        this.handleOnChange=this.handleOnChange.bind(this);
-        this.state={
-textcontent:''
+        this.handleOnChange = this.handleOnChange.bind(this);
+        this.state = {
+            textcontent: ''
         }
 
     }
-    
-    handleOnChange(e:any){
-this.setState({textcontent:e.target.value})
+
+    handleOnChange(e: any) {
+        this.setState({ textcontent: e.target.value })
     }
-render(){
-return(
-    
-         <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-<input onChange={this.handleOnChange}type='text' value ={this.state.textcontent}
+    render() {
+        return (
 
-/>
-<BoilingVerdict celcious={parseFloat(this.state.textcontent)} />
-</fieldset>
+            <fieldset>
+                <legend>Enter temperature in Celsius:</legend>
+                <input onChange={this.handleOnChange} type='text' value={this.state.textcontent}
 
-);
-}
+                />
+                <BoilingVerdict celcious={parseFloat(this.state.textcontent)} />
+            </fieldset>
+
+        );
+    }
 }
 export default Calculator;
