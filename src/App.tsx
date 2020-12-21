31880d13c7555d3bbd57a48946eb1204e1a90fed
aswathyeg <aswathy.eg.bijesh@gmail.com>
2020-12-21 22:39:13 +0530

    added new Login component

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import './App.css';
 // import Clock from './Clock';
 // import Toggle from './Toggle'
 // import { render } from '@testing-library/react';
-// import ConditionalRendering from './ConditionalRendering';
+//import ConditionalRendering from './ConditionalRendering';
 // import Calculator from './Calculator';
 // import Login from './Login';
-import SplitPane from './SplitPane';
-import Contacts from './Contacts';
-import Chat from './Chat';
+// import SplitPane from './SplitPane';
+// import Contacts from './Contacts';
+// import Chat from './Chat';
+ import Login from './Login';
 
 function App() {
   
@@ -20,11 +21,13 @@ function App() {
       <br />
       <Toggle />
       <br />
-      <ConditionalRendering />
+     
       <Calculator /> */}
       {/* <br />
       <Login /> */}
-      <SplitPane left={<Contacts />} right={<Chat />}/>
+     {/*  <SplitPane left={<Contacts />} right={<Chat />}/> */}
+     {/* <ConditionalRendering /> */}
+     <Login />
      </div>
      
    );
