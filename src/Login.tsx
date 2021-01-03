f94cc152ce89828b318ebd49d531b70db4ea9c0c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-01-03 17:43:50 +0530

    splitted login component

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,39 +1,14 @@
-import { Container, TextField } from '@material-ui/core';
-import Button from '@material-ui/core/Button/Button';
-import Typography from '@material-ui/core/Typography';
+import React from 'react';
+import Home from './Home';
+import LoginForm from './LoginForm';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+const Login = () => {
+  return (
+    <Router>
+      <Route exact path="/" component={LoginForm} />
+      <Route path="/home" component={Home} />
 
-import React, { Component } from 'react'
-class Login extends React.Component {
-    constructor(props: any) {
-        super(props);
-        this.state = {
-
-        }
-    }
-    handleChange() { }
-    render() {
-        return (
-            <div>
-                <Container maxWidth="sm">
-
-                    <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
-                    <TextField id="standard-basic" label="Username" /><br />
-                    <TextField
-                        id="filled-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="filled"
-                    /><br />
-                    <Button color="primary">Login</Button>
-
-
-                </Container>
-            </div>
-
-
-        )
-    }
-
+    </Router>
+  );
 }
 export default Login;
