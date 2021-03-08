aa9d3d045115dc93a1e6dd4243d304322e7df408
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-03-08 06:00:31 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
 import { Accordion } from '@material-ui/core';
 import React from 'react';
 import './App.css';
-// import Welcome from './Welcome';
-// import Clock from './Clock';
-// import Toggle from './Toggle'
-// import { render } from '@testing-library/react';
-//import ConditionalRendering from './ConditionalRendering';
-// import Calculator from './Calculator';
-// import Login from './Login';
-// import SplitPane from './SplitPane';
-// import Contacts from './Contacts';
-// import Chat from './Chat';
  import Login from './Login';
 import SimpleAccordion from './SimpleAccordion';
- //import Hooks from './Hooks';
- 
 
-function App() {
+ function App() {
   
     return(
       <div className="App">      
