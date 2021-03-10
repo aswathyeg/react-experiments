9e04f5561fb0db40910ff6814667885e35190e1c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-03-10 06:16:24 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ interface IPost{
     title: string;
     body: string;
 }
-const defaultPosts:IPost[]=[];
+const defaultProps:IPost[]=[];
 
  function App() {
   
-  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultPosts);
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(defaultProps);
   const [loading, setLoading]: [boolean, (loading: boolean) => void] = React.useState<boolean>(true);
   const [error, setError]: [string, (error: string) => void] = React.useState("");
   React.useEffect(() => {
@@ -23,18 +23,33 @@ const defaultPosts:IPost[]=[];
           headers: {
             "Content-Type": "application/json"
           },
+          timeout:1
           
         }).then(response => {
           setPosts(response.data);
           setLoading(false);
-        });;
+        }).catch(ex => {
+          const error =
+          ex.response.status === 404
+            ? "Resource not found"
+            : "An unexpected error has occurred";
+          setError("Error");
+          setLoading(false);
+        });
         
     }, []);
     return(
-      <div className="App">      
-        {/* <Login/>      */}
-        <SimpleAccordion />
-     </div>
+      <div className="App">
+      <ul className="posts">
+        {posts.map((post) => (
+         <li key={post.id}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+         </li>
+       ))}
+      </ul>
+      {error && <p className="error">{error}</p>}
+    </div>
      
    );
     
