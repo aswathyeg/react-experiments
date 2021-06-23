a3b336b6aafa6618db29b2d7ece424b75b641538
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-06-23 16:30:49 +0530

    added usestate hook

--- src/FunctionalComponent.tsx
+++ src/FunctionalComponent.tsx
@@ -2,6 +2,20 @@
 //eg to how functional components handling state
 
 import React, { useState } from 'react'
-function Classroom (props){
-    let[studentCount,setStudentCount]=useState(0);
+
+function Classroom (props :any){
+    let[studentsCount,setStudentCount]=useState(0);//use React hooks to handle state.
+
+    const addStudent=()=>{
+        setStudentCount(++studentsCount)
+    }
+    return(
+        <div>
+            <p>Number of students in classroom:{studentsCount}</p>
+            <button onClick={addStudent}>Add Student</button>
+
+            
+        </div>
+    )
 }
+export default Classroom;
