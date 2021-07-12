3fd5ab4b10b74ae1556976e85b1739c90cbe3e65
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-12 17:50:37 +0530

    example to prevent re-renders

---
+++ src/Child.tsx
@@ -0,0 +1,34 @@
+
+
+import React from 'react';
+
+interface Props {
+
+}
+interface State {
+    message: string
+}
+class Child extends React.Component<Props, State>{
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            message: "This is Vivek"
+        };
+    }
+    // shouldComponentUpdate(){
+    //     console.log("Does not getting rendered")
+    //     return false;
+
+    // } //Prevent re-renders 
+    render() {
+        console.log("Child is getting rendered")
+        return (
+            <div>
+                <p>{this.state.message}</p>
+            </div>
+        )
+    }
+
+}
+export default Child;
