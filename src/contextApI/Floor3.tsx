0d697784670b4eab9cfb579dc4ce5d978ce49785
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-21 18:02:05 +0530

    building components

---
+++ src/contextApI/Floor3.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Floor7 from './Floor7';
+ function Floor3 () {
+    return <Floor7 />;
+}
+export default Floor3;
---
+++ src/contextApI/Floor7.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+ function Floor7 () {
+    
+}
+export default Floor7;
