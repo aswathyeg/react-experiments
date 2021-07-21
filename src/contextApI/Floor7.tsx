27191303a6f42759eac9e4563ad5eb2fb514b9dc
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-21 18:39:54 +0530

    added return

--- src/contextApI/Floor7.tsx
+++ src/contextApI/Floor7.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+import Provider from "./Provider";
+import Context from "./context";
+//import "./styles.css";
+import PackageContext from "./context";
+
+
  function Floor7 () {
+     return (
+         <Context.Consumer>
+ {context => (
+        <Fragment>PackageContext
+          <p>
+            <strong>Recipient Name: </strong>
+          {context.data.recipientName}
+          </p>
+          <p>
+            <strong>Package: </strong>
+            {context.data.package}
+          </p>
+    <p>
+            <strong>Delivery Status: </strong>
+            {context.data.deliveryStatus}
+          </p>
+          <button onClick={context.updateDeliveryStatus}>
+            Update Delivery Status
+          </button>
+        </Fragment>
+      )}
+             
+         </Context.Consumer>
+     )
     
 }
 export default Floor7;
