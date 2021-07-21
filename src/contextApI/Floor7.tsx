2a85408aeef79755137cd091a5d2d01bdcb3310e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-21 19:31:50 +0530

    formatted

--- src/contextApI/Floor7.tsx
+++ src/contextApI/Floor7.tsx
@@ -3,34 +3,32 @@ import ReactDOM from "react-dom";
 import Provider from "./Provider";
 import Context from "./context";
 //import "./styles.css";
-import PackageContext from "./context";
 
-
- function Floor7 () {
-     return (
-         <Context.Consumer>
- {context => (
-        <Fragment>PackageContext
-          <p>
-            <strong>Recipient Name: </strong>
-          {context.data.recipientName}
-          </p>
-          <p>
-            <strong>Package: </strong>
-            {context.data.package}
-          </p>
-    <p>
-            <strong>Delivery Status: </strong>
-            {context.data.deliveryStatus}
-          </p>
-          <button onClick={context.updateDeliveryStatus}>
-            Update Delivery Status
+function Floor7() {
+    return (
+        <Context.Consumer>
+            {context => (
+                <Fragment>PackageContext
+                    <p>
+                        <strong>Recipient Name: </strong>
+                        {context.data.recipientName}
+                    </p>
+                    <p>
+                        <strong>Package: </strong>
+                        {context.data.package}
+                    </p>
+                    <p>
+                        <strong>Delivery Status: </strong>
+                        {context.data.deliveryStatus}
+                    </p>
+                    <button onClick={context.updateDeliveryStatus}>
+                        Update Delivery Status
           </button>
-        </Fragment>
-      )}
-             
-         </Context.Consumer>
-     )
-    
+                </Fragment>
+            )}
+
+        </Context.Consumer>
+    )
+
 }
 export default Floor7;
