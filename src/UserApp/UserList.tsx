38094c8206dcdac712f73f3bdf85d77574df22cb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-31 01:54:00 +0530

    addd link to Home

--- src/UserApp/UserList.tsx
+++ src/UserApp/UserList.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 interface IUserListOwnProps{
 
 }
 export const UserList:React.FC<IUserListOwnProps>=():JSX.Element=>{
     return(
         <div>
-        UserList
+        
+        <p>
+                UserList
+            </p>
+            <Link
+                to='/'
+            >
+                Home
+            </Link>
+
         </div>
     )
 
