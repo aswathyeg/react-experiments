0db2d1f3110d613d0da60b4f0288161f04230157
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-30 23:58:36 +0530

    added event handler

--- src/User.tsx
+++ src/User.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,ChangeEvent} from 'react';
 
 interface IUserOwnProps{ //I + ComponentName + DescriptiveUsage = Interface Name
     username:string|undefined,
@@ -7,6 +7,15 @@ interface IUserOwnProps{ //I + ComponentName + DescriptiveUsage = Interface Name
 
 const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
   const[time,setTime]=useState<Date>(()=>new Date(Date.now())) //added useState
+  const[message,setMessage]=useState<string>('');
+  setInterval(()=>{
+    setTime(new Date(Date.now()));
+  },1000
+    
+  )
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setMessage(event.target.value);
+  };
     return (
         <div className="App">
           Hi, {username ? username : 'Mysterious Entity'}, your user type is 
@@ -14,6 +23,14 @@ const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
           <br></br>
 
           <p>{time.toUTCString()}</p>
+
+          <input type='text'
+          placeholder='Enter your message here'
+          value={message}
+          onChange={handleTextChange}>
+
+          </input>
+          Your message: {message}
         </div>
       );
     
