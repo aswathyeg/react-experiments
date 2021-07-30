30190f274c6647bdb91225c48c0b01cb6b4b9b47
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-31 01:30:25 +0530

    new component

---
+++ src/UserApp/UserList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+interface IUserListOwnProps{
+
+}
+export const UserList:React.FC<IUserListOwnProps>=():JSX.Element=>{
+    return(
+        <div>
+        UserList
+        </div>
+    )
+
+}
