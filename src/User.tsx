99b9ac991ae15de486549809ea92be9e7fcb5010
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-30 23:05:33 +0530

    updated

--- src/User.tsx
+++ src/User.tsx
@@ -3,7 +3,7 @@ interface IUserOwnProps{ //I + ComponentName + DescriptiveUsage = Interface Name
     username:string|undefined,
     userType:'admin' | 'moderator' | 'user' | 'guest';
 }
-const User:React.FC<IUserOwnProps>= ({username,userType}) =>{
+const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
     return (
         <div className="App">
           Hi
