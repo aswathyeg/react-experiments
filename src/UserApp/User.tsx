b60e3ffa88f37bd18c1003038376407f0569b74f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-31 01:30:03 +0530

    added useeffect

--- src/UserApp/User.tsx
+++ src/UserApp/User.tsx
@@ -1,21 +1,29 @@
-import React, { useState,ChangeEvent} from 'react';
+import React, { useState,ChangeEvent, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 interface IUserOwnProps{ //I + ComponentName + DescriptiveUsage = Interface Name
     username:string|undefined,
     userType:'admin' | 'moderator' | 'user' | 'guest';
 }
 
-const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
-  const[time,setTime]=useState<Date>(()=>new Date(Date.now())) //added useState
-  const[message,setMessage]=useState<string>('');
-  setInterval(()=>{
-    setTime(new Date(Date.now()));
-  },1000
-    
-  )
+const User: React.FC<IUserOwnProps> = ({ userType, username }): JSX.Element => {
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+  const [message, setMessage] = useState<string>('');
+
   const handleTextChange = (event: ChangeEvent<HTMLInputElement>): void => {
     setMessage(event.target.value);
   };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date(Date.now()));
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    }
+  }, [username]);
+
     return (
         <div className="App">
           Hi, {username ? username : 'Mysterious Entity'}, your user type is 
@@ -31,6 +39,8 @@ const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
 
           </input>
           Your message: {message}
+          <br></br>
+          <Link to='/UserList'> User List</Link>
         </div>
       );
     
