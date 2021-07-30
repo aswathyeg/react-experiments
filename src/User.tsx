96bdc188801f24733835a723213e1440d644ce97
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-07-30 23:29:23 +0530

    added useState

--- src/User.tsx
+++ src/User.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 interface IUserOwnProps{ //I + ComponentName + DescriptiveUsage = Interface Name
     username:string|undefined,
     userType:'admin' | 'moderator' | 'user' | 'guest';
 }
+
 const User:React.FC<IUserOwnProps>= ({username,userType}): JSX.Element =>{
+  const[time,setTime]=useState<Date>(()=>new Date(Date.now()))
     return (
         <div className="App">
-          Hi, {username ? username : 'Mysterious Entity'}, your user type is {username ? userType : 'irrelevant because I do not know you'}.
+          Hi, {username ? username : 'Mysterious Entity'}, your user type is 
+          {username ? userType : 'irrelevant because I do not know you'}.
+          <br></br>
+
+          <p>{time.toUTCString()}</p>
         </div>
       );
     
