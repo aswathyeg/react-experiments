bf4d41155d2fb9197e16b2c603df7afbb51cf775
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-08-01 17:47:35 +0530

    UserActions component

---
+++ src/UserApp/user/UserActions.ts
@@ -0,0 +1,20 @@
+import {
+    IUser,
+    UserActions,
+    IUserActionTypes
+
+} from './UserTypes';
+
+export function saveUsername(user:IUser):IUserActionTypes{//used ducktyping here
+    return{
+    type:UserActions.SAVE_USERNAME,
+    payload:user
+};
+}
+export function saveUserMessage(user:IUser):IUserActionTypes{
+    return{
+    type:UserActions.SAVE_USER_MESSAGE,
+    payload:user
+
+};
+}
