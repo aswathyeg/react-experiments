7d22c24b1e4a86e9bf24555f5e591d6f7fc9d878
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-08-16 10:59:46 +0530

    new component

---
+++ src/LoginHere.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MDBInput } from "mdbreact";
+
+const InputPage = () => {
+  return (
+    <MDBInput label="E-mail address" outline icon="envelope" />
+  );
+}
+
+export default InputPage;
