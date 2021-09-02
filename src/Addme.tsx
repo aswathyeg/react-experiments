949f04ada4ff3004e5761bcef7ac774d87f87c19
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-02 11:33:55 +0530

    removed code for api fetch

--- src/Addme.tsx
+++ src/Addme.tsx
@@ -1,27 +1,36 @@
 
 import { Button } from '@material-ui/core';
 import React, { useState } from 'react';
+import 'axios' ;
 function AddMe(){
+    const axios = require('axios').default;//for intellisence
+
+    // const instance = axios.create({ //created custom config
+    //     baseURL: 'https://some-domain.com/api/',
+    //     timeout: 1000,
+    //     headers: {'X-Custom-Header': 'foobar'}
+    //   });
+
+      
 
     let [count,setCount]=useState(0);
     const increment=()=>{
         setCount(++count)
+        console.log('foo');
 
     }
     const decrement=()=>{
         setCount(--count)
     }
-   const fetchData=()=>{
 
-
-    }
+    
 return(
     <div>
        <Button> Current number:{count}</Button>
        <br></br>
     <Button onClick={increment}> increment</Button>
     <Button onClick={decrement}> decrement</Button>
-    <Button onClick={fetchData}>fetchData</Button>
+    
     
     </div>
 )
