f7935ce8861ab63a88b58080719663d53071a779
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-09 17:42:25 +0530

    rendered component changed

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import Hello from './Hello';
 import PropsContext from './Props';
 import Props from './Props';
 import ContextExample from './ContextExample';
+import DetectHover from './HOCExample';
 
 
 
@@ -62,7 +63,7 @@ import ContextExample from './ContextExample';
 function App() {
   return (
     <div>
-   <ContextExample/>
+   <DetectHover/>
    
      </div>
   )
