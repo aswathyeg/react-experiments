aeb530b7bd0bd81c3a804dd8872713fe0b24d1b2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-12 23:49:00 +0530

    copy of calculator component

---
+++ src/Calculators.tsx
@@ -0,0 +1,28 @@
+
+import React from 'react';
+interface Props{
+
+}
+interface State{
+    
+}
+export default class Caluculators extends React.Component<Props,State>{
+    constructor(props){
+        super(props);
+        this.state={
+
+        }
+    }
+    render (){
+        return(
+            <div>
+                <fieldset>
+<legend>Enter Temperature in celsious</legend>
+
+                </fieldset>
+
+            </div>
+        )
+    }
+
+}
