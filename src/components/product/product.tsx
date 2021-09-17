1f2d17f1afbdea515357b5e3949a65b287bf92c0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-18 01:30:35 +0530

    added function to add and remove

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './product.css';
-export default function Product(){
-    return(
+
+const currencyOptions = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+}
+const getTotal = (total) => {
+    return total.toLocaleString(undefined, currencyOptions)
+
+}
+
+export default function Product() {
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+
+
+    const addMore = () => {
+        setCart(['IceCream']);
+        setTotal(5);
+
+    }
+    const remove = () => {
+        setCart([]);
+        setTotal(0);
+    }
+
+
+    return (
         <div className="wrapper">
-        <div>
-          Shopping Cart: 0 total items.
+            <div>
+                Shopping Cart: {cart.length} total items.
         </div>
-        <div>Total: 0</div>
-  
-        <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
-        <button>Add</button> <button>Remove</button>
-      </div>
+            <div>Total: {getTotal(total)}</div>
+
+            <div className="product"><span role="img" aria-label="ice cream">🍦</span></div>
+            <button onClick={addMore}>Add</button> <button onClick={remove}>Remove</button>
+        </div>
     )
 }
