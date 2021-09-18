1e72edbf5e3e507cda5625303120aff01f636712
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-18 22:21:12 +0530

    formatted

--- src/useHooks/Counter.tsx
+++ src/useHooks/Counter.tsx
@@ -1,38 +1,38 @@
 //eg to useReducer
 
 import *as React from "react";
-const initialState={count:0};
-type ACTIONTYPE=
-| { type: 'increment', payload: number} 
-  | { type: 'decrement', payload: string} 
+const initialState = { count: 0 };
+type ACTIONTYPE =
+  { type: 'increment', payload: number }
+  | { type: 'decrement', payload: string }
 
-function reducer(state:typeof initialState,action: ACTIONTYPE ){
-    switch (action.type) {
-        case 'increment':
-          return {count: state.count + action.payload};
-        case 'decrement':
-          return {count: state.count - Number(action.payload)};
-        default:
-          throw new Error();
-      }
+function reducer(state: typeof initialState, action: ACTIONTYPE) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + action.payload };
+    case 'decrement':
+      return { count: state.count - Number(action.payload) };
+    default:
+      throw new Error();
+  }
 }
 
-function Counter (){
-    const [state,dispatch]=React.useReducer(reducer,initialState);
+function Counter() {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
 
-    const substract=()=>{
-      dispatch({type:'decrement',payload:'5'})
-    }   
-    const add=()=>{
-      dispatch({type:'increment',payload:5})
-    }   
-    
-    return(
-        <>
-        Count:{state.count}
-        <button onClick={substract}>-</button>
-        <button onClick={add}>+</button>
-        </>
-    );
+  const substract = () => {
+    dispatch({ type: 'decrement', payload: '5' })
+  }
+  const add = () => {
+    dispatch({ type: 'increment', payload: 5 })
+  }
+
+  return (
+    <>
+      Count:{state.count}
+      <button onClick={substract}>-</button>
+      <button onClick={add}>+</button>
+    </>
+  );
 }
 export default Counter;
