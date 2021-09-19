13a417d3d0b4d1332c93d2340d0ee7bb81fe9f11
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-20 01:52:50 +0530

    added Link

--- src/components/routing/Landing.tsx
+++ src/components/routing/Landing.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Manatee from './Manatee/Manatee';
 import './Landing.css';
-import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import {BrowserRouter,Switch,Route, Link} from 'react-router-dom'
 import Narwhal from './Narwhal/Narwhal';
 import Whale from './Whale/Whale';
 export default function Landing(){
@@ -9,9 +9,18 @@ export default function Landing(){
         <div className="wrapper">
         <h1>Marine Mammals</h1>
         <BrowserRouter>
+<nav>
+    <ul>
+    <li> <Link to="/manatee">Manatee</Link></li>
+  <li> <Link to="/narwhal">Narwhal</Link></li>
+  <li> <Link to="/whale">Whale</Link></li>
+    </ul>
+</nav>
+
+
 <Switch>
 
-    <Route path="/"><Manatee /></Route>
+    <Route path="/manatee"><Manatee /></Route>
 
     <Route path="/narwhal"><Narwhal /></Route>
     <Route path="/whale"><Whale /></Route>
