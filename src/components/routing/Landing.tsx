2cc5437507bd7032b9ba29df6ede121ce1665b73
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-20 00:32:17 +0530

    added new components for routing

---
+++ src/components/routing/Landing.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Manatee from './Manatee/Manatee';
+export default function Landing(){
+    return(
+        <div className="wrapper">
+        <h1>Marine Mammals</h1>
+        <Manatee />
+      </div>
+    
+    )
+}
---
+++ src/components/routing/Whale/Whale.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default function Whale(){
+    return(
+        <h2>
+            Whale
+        </h2>
+    
+    )
+}
