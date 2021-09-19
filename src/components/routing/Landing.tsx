88667524b085cacd27ff6fcc8be366235db579ee
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-20 01:16:43 +0530

    added brouserRouter

--- src/components/routing/Landing.tsx
+++ src/components/routing/Landing.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Manatee from './Manatee/Manatee';
+import './Landing.css';
+import {BrowserRouter,Switch,Route} from 'react-router-dom'
+import Narwhal from './Narwhal/Narwhal';
+import Whale from './Whale/Whale';
 export default function Landing(){
     return(
         <div className="wrapper">
         <h1>Marine Mammals</h1>
-        <Manatee />
+        <BrowserRouter>
+<Switch>
+
+    <Route path="/"><Manatee /></Route>
+
+    <Route path="/narwhal"><Narwhal /></Route>
+    <Route path="/whale"><Whale /></Route>
+</Switch>
+
+
+        </BrowserRouter>
+        
       </div>
     
     )
