f14c80bfa61b7f05ab5f91f5b5c4a8b538921fb9
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-22 00:40:36 +0530

    imported added proptypes

--- src/components/LoginAuthentication/Login/Login.tsx
+++ src/components/LoginAuthentication/Login/Login.tsx
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
-export default function Login(props){
+import PropTypes from 'prop-types';
+export default function Login({setToken}){
+    const[username,setUsername]=useState('');
+    const [password,setPassword]=useState('');
     return(
         <div >
             <h1 className="login-wrapper">
                 Please Login
             </h1>
             <form className="login-wrapper">
-            <input type="text" placeholder="UserName"/>
+            <input type="text" placeholder="UserName" onChange={e=>setUsername(e.target.value)}/>
             <br></br>
-            <input type="password" placeholder="password"/>
+            <input type="password" placeholder="password" onChange={e=>setPassword(e.target.value)}/>
             <br></br>
             <button type="submit">Submit</button>
             </form>
         </div>
     )
 }
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+  }
