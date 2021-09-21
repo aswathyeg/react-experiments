0fa0eb6b1a63b6c902701630046a7cbf95837dbc
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-22 00:40:04 +0530

    added sample token

--- server.tsx
+++ server.tsx
@@ -5,9 +5,9 @@ const cors=require('cors');//express and cors are used for creating fake server
 const app = express();
 
 app.use(cors());
-app.use('../login',(req,res)=>{
-res.send({
-    token:'test123'
-})
-});
+app.use('/login', (req, res) => {
+    res.send({
+      token: 'test123'
+    });
+  });
 app.listen(8080,()=>console.log('API is running on http://localhost:8080/login'));
