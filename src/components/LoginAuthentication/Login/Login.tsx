784304f53b9cce4c086631e6f492a304f4a5584d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-22 00:52:56 +0530

    added function to post data

--- src/components/LoginAuthentication/Login/Login.tsx
+++ src/components/LoginAuthentication/Login/Login.tsx
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
+
+async function loginUser(credentials) {
+return fetch('http://localhost:8080/login',{
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json'},
+    body:JSON.stringify(credentials)
+})
+.then(data=>data.json())}
+
+
+
 export default function Login({setToken}){
     const[username,setUsername]=useState('');
     const [password,setPassword]=useState('');
