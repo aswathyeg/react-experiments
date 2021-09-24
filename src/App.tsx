4ed6f0f9cd570bd7d171434846c21fd057724455
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-24 22:53:38 +0530

    cleaned up

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
 
 // list and props example
-import LastFloor from './Reducer/LastFloor';
+
 import React from 'react';
-import Provider from './contextApI/Provider';
-import Building from './contextApI/Building';
-import UserDetails from './UserApp/UserDetails';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { UserList } from './UserApp/UserList';
-import LoginHere from './LoginHere';
-import AddMe from './Addme';
-import AddMeClass from './AddMeClass';
-import Addme from './Addme';
-import PropsState from './PropsState';
-import LiftStateUp from './LiftStateUp';
-import ControlledUncontrolled from './Homee';
-//import TemperatureInput from './Homee';
-//import Home from './Homee';
-import Homee from './Homee';
-import Refs from './Refs';
-import KeyExample from './KeyExample';
-import Hello from './Hello';
-import PropsContext from './Props';
-import Props from './Props';
-import ContextExample from './ContextExample';
-import DetectHover from './HOCExample';
-import IncrementDecrementFunction from './IncrementDecrementFunction';
-import LifeCycleMethods from './LifeCycleMethods';
-import Calculator from './Calculator';
-import Calculators from './Calculators';
-import Data from './customize components with props/Data';
-import Product from  './components/product/product';
-import Landing from './components/routing/Landing';
 import LandLogin from './components/LoginAuthentication/LandLogin';
 
 function App() {
