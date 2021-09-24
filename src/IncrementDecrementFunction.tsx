594efe1a362d51bc70cccc353091e8a8971a72f5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-24 22:59:03 +0530

    added header for testing

--- src/IncrementDecrementFunction.tsx
+++ src/IncrementDecrementFunction.tsx
@@ -14,6 +14,7 @@ function Counting() {
 
     return (
         <div>
+            <h1>This is counter app</h1> //for testing
             <p>Current Value:{count}</p>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
