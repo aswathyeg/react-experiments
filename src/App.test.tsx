da01108775f9605595714ae577f4a42307f59245
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-25 11:25:58 +0530

    added shallow to enzyme import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import React from 'react';
 import IncrementDecrementFunction from './IncrementDecrementFunction';
-import { configure } from 'enzyme';
+import { configure,shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
