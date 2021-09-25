ef4b6e68d34f6b1134fb32e1ea216953d78a2c51
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-26 00:19:56 +0530

    extract code for wrapper(beforeEach)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,15 +7,22 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('counter testing',()=>{
+    let wrapper;
+    beforeEach(()=>{
+        wrapper=shallow(<IncrementDecrementFunction/>); 
+    })
       
     test("render the title of the counter",()=>{
-        const wrapper=shallow(<IncrementDecrementFunction/>);
+       
         expect(wrapper.find("h1").text()).toContain("This is counter app");
     });
     
     test("render a buttun with text of `Increment~",()=>{
-        const wrapper=shallow(<IncrementDecrementFunction/>);
+        
         expect (wrapper.find("#increment-btn").text()).toBe('Increment');
     });
     
+    test("render the initial value of state in a div",()=>{
+
+    })
 });
