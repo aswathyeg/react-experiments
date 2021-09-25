d890fb83c679fecad66db3c655b4b2b7efb3d81b
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-26 00:56:39 +0530

    added test for counter

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -21,8 +21,14 @@ describe('counter testing',()=>{
         
         expect (wrapper.find("#increment-btn").text()).toBe('Increment');
     });
-    
+
     test("render the initial value of state in a div",()=>{
+        expect(wrapper.find("#counter-value").text()).toBe("0");
+
+    });
+    test("render the click event of increment button and increment counter value",()=>{
+        wrapper.find("#increment-btn").simulate("click");
+        expect(wrapper.find("#counter-value").text()).toBe("1");
 
     })
 });
