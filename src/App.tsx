cfda1aede3c28483ad78228dfccf84d9ff12e20e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-09-30 23:32:56 +0530

    rendered animalcard component

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,26 @@
 import React from 'react';
 import ClassLifecycle from './ClassLifecycle';
 import UseEffectExample from './UseEffectExample';
+import animals from './customize components with props/Data';
+import AnimalCard from './customize components with props/AnimalCard';
 
 function App() {
 
+
   return (
-  <div>
-  <ClassLifecycle />
-  </div>
+    <div>
+      {animals.map(animal => {
+        <AnimalCard
+          diet={animal.diet}
+          scientificName={animal.scientificName}
+          name={animal.name}
+          size={animal.size}
+
+        />
+
+      })}
+
+    </div>
   )
 }
 
