b2fb938f3a898256859c8ec9d6f22c0c5035cd73
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-01 16:25:32 +0530

    form example

---
+++ src/components/FormExample.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+function FormExample(){
+    return(
+        <div className="wrapper">
+            <h1>
+                How about the Apples
+            </h1>
+            <form>
+                <fieldset>
+<label>
+
+</label>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
