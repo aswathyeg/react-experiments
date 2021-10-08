0faecbc39cbd1309d771b753d6faf093d4c7286f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-09 00:41:08 +0530

    added sideBar

--- src/SecondTeam.tsx
+++ src/SecondTeam.tsx
@@ -1,98 +1,116 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Avatar, Divider, Tooltip } from 'antd';
-import { UserOutlined, AntDesignOutlined } from '@ant-design/icons';
-const SecondTeam = () => (
-    <>
-      <Avatar.Group>
-        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        <Avatar
-          style={{
-            backgroundColor: '#f56a00',
-          }}
-        >
-          K
+import { Layout, Menu, Breadcrumb, Avatar } from 'antd';
+import { useHistory } from 'react-router-dom';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import { Redirect } from 'react-router-dom'
+
+const { Header, Content, Footer, Sider } = Layout;
+const { SubMenu } = Menu;
+
+
+
+export default class secondTeam extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.clickStatus = this.clickStatus.bind(this);
+    this.clickFirstTeam = this.clickFirstTeam.bind(this);
+    this.clickSecondTeam = this.clickSecondTeam.bind(this);
+  }
+
+  state = {
+    collapsed: false,
+    statusRedirect: false,
+    firstTeamRedirect: false,
+    secondTeamRedirect: false
+  };
+
+  onCollapse = collapsed => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  };
+  clickStatus() {
+    this.setState({ statusRedirect: true });
+
+
+  }
+  clickFirstTeam() {
+    this.setState({ firstTeamRedirect: true });
+
+  }
+  clickSecondTeam() {
+    this.setState({ secondTeamRedirect: true });
+
+  }
+  render() {
+
+    const { collapsed } = this.state;
+    if (this.state.statusRedirect) {
+
+      return <Redirect push to="/status" />;
+
+
+    }
+    if (this.state.firstTeamRedirect) {
+
+      return <Redirect push to="/firstTeam" />;
+
+
+    }
+    if (this.state.secondTeamRedirect) {
+
+      return <Redirect push to="/secondTeam" />;
+
+
+    }
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1" onClick={this.clickStatus} icon={<PieChartOutlined />}>
+              Team Summary
+                </Menu.Item>
+            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team Details">
+              <Menu.Item key="6" onClick={this.clickFirstTeam}>Team 1</Menu.Item>
+              <Menu.Item key="8" onClick={this.clickSecondTeam}>Team 2</Menu.Item>
+            </SubMenu>
+
+          </Menu>
+        </Sider>
+        <>
+
+          <div id="container" style={{ padding: "44px" }}>
+
+
+            <Avatar.Group>
+
+              <Avatar
+                style={{
+                  backgroundColor: '#f56a00',
+                }}
+              >
+                2
         </Avatar>
-        <Tooltip title="Ant User" placement="top">
-          <Avatar
-            style={{
-              backgroundColor: '#87d068',
-            }}
-            icon={<UserOutlined />}
-          />
-        </Tooltip>
-        <Avatar
-          style={{
-            backgroundColor: '#1890ff',
-          }}
-          icon={<AntDesignOutlined />}
-        />
-      </Avatar.Group>
-      <Divider />
-      <Avatar.Group
-        maxCount={2}
-        maxStyle={{
-          color: '#f56a00',
-          backgroundColor: '#fde3cf',
-        }}
-      >
-        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        <Avatar
-          style={{
-            backgroundColor: '#f56a00',
-          }}
-        >
-          K
-        </Avatar>
-        <Tooltip title="Ant User" placement="top">
-          <Avatar
-            style={{
-              backgroundColor: '#87d068',
-            }}
-            icon={<UserOutlined />}
-          />
-        </Tooltip>
-        <Avatar
-          style={{
-            backgroundColor: '#1890ff',
-          }}
-          icon={<AntDesignOutlined />}
-        />
-      </Avatar.Group>
-      <Divider />
-      <Avatar.Group
-        maxCount={2}
-        size="large"
-        maxStyle={{
-          color: '#f56a00',
-          backgroundColor: '#fde3cf',
-        }}
-      >
-        <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        <Avatar
-          style={{
-            backgroundColor: '#f56a00',
-          }}
-        >
-          K
-        </Avatar>
-        <Tooltip title="Ant User" placement="top">
-          <Avatar
-            style={{
-              backgroundColor: '#87d068',
-            }}
-            icon={<UserOutlined />}
-          />
-        </Tooltip>
-        <Avatar
-          style={{
-            backgroundColor: '#1890ff',
-          }}
-          icon={<AntDesignOutlined />}
-        />
-      </Avatar.Group>
-    </>
-  );
-   export default SecondTeam;;
+
+
+            </Avatar.Group>
+
+          </div>
+        </>
+
+      </Layout>
+    );
+  }
+}
+
