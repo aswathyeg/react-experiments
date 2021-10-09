868a8489d7ba0970720b18f05d5e332fad436bb4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-10 01:11:21 +0530

    component name changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,34 @@
-
-// list and props example
-
-import React from 'react';
-import ClassLifecycle from './ClassLifecycle';
-import UseEffectExample from './UseEffectExample';
-import animals from './customize components with props/Data';
-import AnimalCard from './customize components with props/AnimalCard';
-import FormExample from './components/FormExample';
-import Counting from './Counting';
-import RiverInformation from './RiverInformation';
-import KeyExample from './KeyExample';
-import Keys from './Keys';
-import Keyexample from './Keyexamples';
-import KeyBlog from './KeyBlog';
-
-import SideBar from './SiderBar';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Status from './Status';
 import FirstTeam from './FirstTeam';
 import SecondTeam from './SecondTeam';
+import TeamSummary from './TeamSummary';
+import LandingSider from './LandingSider';
 
 function App() {
 
   return (
-   
-    
-    
-    
     <BrowserRouter>
-     <div>
-      
-        <Switch>
-          <Route exact path='/' component={SideBar}></Route>
-        </Switch>
-
+      <div>
 
         <Switch>
-          <Route path='/status' component={Status}></Route>
-        </Switch>
-        <Switch>
+          <Route exact path='/' component={LandingSider}></Route>
+
+
+          <Route path='/teamSummary' component={TeamSummary}></Route>
+
+
           <Route path='/firstTeam' component={FirstTeam}></Route>
-        </Switch>
-        <Switch>
+
+
           <Route path='/secondTeam' component={SecondTeam}></Route>
         </Switch>
 
-        </div>
+      </div>
 
     </BrowserRouter>
-     
-    
-    
+
+
+
   )
 }
 
