a65e88beae76691640f3c1b2f58d13f659d0bf96
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-15 10:12:14 +0530

    added chart component

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,42 @@ import FirstTeam from './RoutingExample/FirstTeam';
 import SecondTeam from './RoutingExample/SecondTeam';
 import TeamSummary from './RoutingExample/TeamSummary';
 import LandingSider from './RoutingExample/LandingSider';
+import { useEffect ,useState} from 'react';
+import  Chart  from "./components/Chart";
 
+      
 function App() {
+  
+  useEffect(()=>{
+    const dashboard=async ()=>{
+      const res=await fetch('http://localhost:8080/dashboards')
+      const data=await res.json()
+      setChartData({
+        ClassId:data.data.map((crypto) => crypto.ClassId),
+        datasets: [
+          {
+            ClassId: 101,
+            data: data.data.map((crypto) => crypto.StudentCount),
+            backgroundColor: [
+              "#ffbb11",
+              "#ecf0f1",
+              "#50AF95",
+              "#f3ba2f",
+              "#2a71d0"
+            ]
+          }
+        ]
+      });
+    };
+    dashboard ()
+  },[]);
+ 
+  const [chartData, setChartData]=useState({})
 
   return (
-    <BrowserRouter>
-      <div>
-
-        <Switch>
-          <Route exact path='/' component={LandingSider}></Route>
-
-
-          <Route path='/teamSummary' component={TeamSummary}></Route>
-
-
-          <Route path='/firstTeam' component={FirstTeam}></Route>
-
-
-          <Route path='/secondTeam' component={SecondTeam}></Route>
-        </Switch>
-
-      </div>
-
-    </BrowserRouter>
+    <div className="App">
+      <Chart chartData={chartData} />
+     </div>
 
 
 
