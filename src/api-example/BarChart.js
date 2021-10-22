18d57b5c0f82f1021a5f09c89525e94d28e49f71
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-22 10:38:29 +0530

    updated

---
+++ src/api-example/BarChart.js
@@ -0,0 +1,117 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { Bar } from 'react-chartjs-2';
+
+export default function BarChart() {
+
+    const [chartData, setChartData] = useState({
+
+
+    });
+    useEffect(() => {
+        fetchDashboard();
+
+    }, [])
+
+    const fetchDashboard = async () => {
+        let id = [];
+        let count = [];
+        await axios.get('http://localhost:8080/dashboards')
+            .then(res => {
+                console.log(res);
+                for (const dataObj of res.data) {
+                    id.push(parseInt(dataObj.class_id));
+                    count.push(parseInt(dataObj.student_count));
+
+                }
+                setChartData({
+                    labels: count,
+                    datasets: [{
+                        label: 'level of thicceness',
+                        data: id,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                        ],
+                        borderWidth: 1
+                    }]
+                });
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }
+    return (
+        <div className="App">
+            <h1>Bar Chart</h1>
+            <div>
+                <Bar
+                    data={chartData}
+                    options={{
+                        responsive: true,
+                        title: { text: "THICCNESS SCALE", display: true },
+                        scales: {
+                            yAxes: {
+                                ticks: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    }}
+                />
+            </div>
+        </div>
+    )
+
+}
+
+
