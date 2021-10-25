108cf71fc6efc206ef3b450a966439b5f2f23a97
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-26 01:03:35 +0530

    added and next and previous page

--- src/Pagination.js
+++ src/Pagination.js
@@ -33,13 +33,18 @@ render(){
     const indexOfLastPost=currentPage*postsPerPage;
     const indexOfFirstPost=indexOfLastPost-postsPerPage;
     const currentPosts=posts.slice(indexOfFirstPost,indexOfLastPost);
+    const paginate=pageNum => this.setState({currentPage:pageNum})
+    const nextPage=()=>this.setState({currentPage:currentPage+1})
+    const prevPage=()=>this.setState({currentPage:currentPage-1})
+    
+    
 
     return(
         <div className="container">
             <h1 className="my-5 text-primary text-center">Pagination</h1>
 
             <Posts posts={currentPosts} loading={loading}/>
-            <PaginationExample postsPerPage ={postsPerPage} totalPosts={posts.length} />
+            <PaginationExample postsPerPage ={postsPerPage} totalPosts={posts.length} paginate={paginate} nextPage={nextPage} prevPage={prevPage}/>
 
         </div>
     )
