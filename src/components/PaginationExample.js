909adf5cf636dde3c9c18294af9bd69494802d55
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-26 00:51:49 +0530

    added onclick as paginate

--- src/components/PaginationExample.js
+++ src/components/PaginationExample.js
@@ -1,14 +1,34 @@
 import React from 'react';
 export default class PaginationExample extends React.Component{
     render(){
-        const{ postsPerPage,totalPosts}=this.props;
+        const{ postsPerPage,totalPosts,paginate}=this.props;
         const pageNumbers=[];
         for(let i=1;i<Math.ceil(totalPosts/postsPerPage);i++){
             pageNumbers.push(i);
         }
         return(
             <div>
+<nav>
+    <ul className="pagination justify-content-center">
 
+        <li className="page-item">
+            <a className="page-link" href="#">Previous</a>
+
+        </li>
+        {pageNumbers.map(num=>(
+
+            <li className="page-item" key={num}>
+                <a onClick={()=>paginate(num)} href="#" className="page-link">
+                    {num}
+                </a>
+            </li>
+        ))}
+        <li className="page-item">
+            <a className="page-link" href="#">Next</a>
+
+        </li>
+    </ul>
+</nav>
             </div>
         )
     }
