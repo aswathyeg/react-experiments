7ede485be43c23c7230ccc1119affb9f044f721d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-26 01:05:58 +0530

    added next and previous page functionality

--- src/components/PaginationExample.js
+++ src/components/PaginationExample.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export default class PaginationExample extends React.Component{
     render(){
-        const{ postsPerPage,totalPosts,paginate}=this.props;
+        const{ postsPerPage,totalPosts,paginate,nextPage,prevPage}=this.props;
         const pageNumbers=[];
         for(let i=1;i<Math.ceil(totalPosts/postsPerPage);i++){
             pageNumbers.push(i);
@@ -12,13 +12,13 @@ export default class PaginationExample extends React.Component{
     <ul className="pagination justify-content-center">
 
         <li className="page-item">
-            <a className="page-link" href="#">Previous</a>
+            <a className="page-link" href="#" onClick={()=>prevPage()}>Previous</a>
 
         </li>
         {pageNumbers.map(num=>(
 
             <li className="page-item" key={num}>
-                <a onClick={()=>paginate(num)} href="#" className="page-link">
+                <a onClick={()=>paginate(num)} href="#" onClick={()=>nextPage()}className="page-link">
                     {num}
                 </a>
             </li>
