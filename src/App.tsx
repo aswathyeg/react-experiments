9736191d42317f4926dccec547bbb1da9a0ab1cb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-25 17:10:12 +0530

    added pagination component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import FirstTeam from './RoutingExample/FirstTeam';
-import SecondTeam from './RoutingExample/SecondTeam';
-import TeamSummary from './RoutingExample/TeamSummary';
-import LandingSider from './RoutingExample/LandingSider';
-import React, { useEffect ,useState} from 'react';
-import BarChart from './api-example/BarChart'
 
-import Chart from './components/Chart'
-
-import ApiExample from './api-example/ApiExample';
-import Table from './Table';
+import  Pagination from './Pagination';
 
 
       
 function App() {
     return (
     <div className="App">
-    <Table/>
+    <Pagination/>
      </div>
 
 
