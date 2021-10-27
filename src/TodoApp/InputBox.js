e34fbd07e5b46ae9e09880a15d3f4da6a5b483eb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-27 16:45:20 +0530

    component for addnew

---
+++ src/TodoApp/InputBox.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function InputBox(props){
+    const  {title}=props;
+    
+        return(
+           
+                <input type="text" className="form-control add-todo" placeholder="Add New"></input>
+                )}
