c49b09120c852a4c4773a9d7a6ea2f4aa453234e
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-28 00:05:49 +0530

    removed inputbox

--- src/TodoApp/TodoList.js
+++ src/TodoApp/TodoList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import TodoItem from './TodoItem';
 import Footer from './Footer';
-import InputBox from './InputBox';
+
 
 export default function TodoList(props){
     const  {title,list}=props;
@@ -16,7 +16,7 @@ export default function TodoList(props){
                 {list.map(item => <TodoItem key={item.id} data={item}/>)}
             </ul>
             <Footer count={count}/>
-            <InputBox/>
+            
             </div>
         )
     }
