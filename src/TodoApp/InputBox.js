414c1872537c424d7bf719f6c302308b7a9eb4ae
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-28 00:48:53 +0530

    added event handlers

--- src/TodoApp/InputBox.js
+++ src/TodoApp/InputBox.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function InputBox(props){
-    const  {title}=props;
-    
+export default class InputBox extends Component{
+
+    constructor(){
+        super();
+        this.state={
+value :props.value||''
+
+        }
+    }
+    handleKeyUp(e){
+        if (e.keyCode === KeyCode.KEY_RETURN) {
+            // Add new Todo Here
+            // Clear the text box
+        }
+
+    }
+    handleChange(e){
+        this.setState(e.target.value)
+    }
+    render(){
         return(
            
-                <input type="text" className="form-control add-todo" placeholder="Add New"></input>
-                )}
+                <input type="text" className="form-control add-todo" 
+                placeholder="Add New"
+                 value={this.state.value}
+                 onKeyUp={this.handleKeyUp.bind(this)}
+onChange={this.handleChange.bind(this)}
+
+                >
+
+                </input>
+        )
+    }
+                }
