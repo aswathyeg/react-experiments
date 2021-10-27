3ca5a263c038516c9627927f40f88dc06ff0efb8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-28 01:38:42 +0530

    added clear and filled handle events

--- src/TodoApp/InputBox.js
+++ src/TodoApp/InputBox.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 
+import KeyCode from 'keycode';
+
 export default class InputBox extends Component{
 
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
 value :props.value||''
 
         }
     }
+    handleChange(e){
+        this.setState({value:e.target.value});
+    }
+    clear(){
+        this.setState({value:''})
+    }
     handleKeyUp(e){
-        if (e.keyCode === KeyCode.KEY_RETURN) {
+        const{ addNewItem}=this.props;
+        const text=this.state.value.trim();
+        if (e.keyCode === KeyCode.KEY_RETURN && text) {
+            addNewItem(text);
+            this.clear();
             // Add new Todo Here
             // Clear the text box
         }
 
     }
-    handleChange(e){
-        this.setState(e.target.value)
-    }
+   
     render(){
+
         return(
            
                 <input type="text" className="form-control add-todo" 
-                placeholder="Add New"
+                 placeholder="Add New"
                  value={this.state.value}
                  onKeyUp={this.handleKeyUp.bind(this)}
-onChange={this.handleChange.bind(this)}
+                 onChange={this.handleChange.bind(this)}
 
                 >
 
