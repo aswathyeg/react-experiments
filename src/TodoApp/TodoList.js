0a36fece89d23f9b053da3d0ac11a22b76052dee
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-27 16:45:42 +0530

    added addnew

--- src/TodoApp/TodoList.js
+++ src/TodoApp/TodoList.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import Header from './Header';
+import TodoItem from './TodoItem';
+import Footer from './Footer';
+import InputBox from './InputBox';
+
 export default function TodoList(props){
-    const  {title,list}=props
+    const  {title,list}=props;
+    const count  =list.length; 
     
         return(
             <div  className="todolist">
-                <Header />
+                <Header title={title} />
 
                 <ul className="list-unstyled">
-{list.map(item=>(
-      <li key={item.id} className="ui-state-default">
-
-<div className="checkbox">
-                            <label>
-                                <input type="checkbox" value=""/> {item.text}
-                            </label>
-                        </div>
-  </li> 
-))}
-
-</ul>
+                {list.map(item => <TodoItem key={item.id} data={item}/>)}
+            </ul>
+            <Footer count={count}/>
+            <InputBox/>
             </div>
         )
     }
