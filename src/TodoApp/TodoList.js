660f4af1b61b179a32464b2379969da81a802e66
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-29 17:07:26 +0530

    added elements to Footer component

--- src/TodoApp/TodoList.js
+++ src/TodoApp/TodoList.js
@@ -5,18 +5,35 @@ import Footer from './Footer';
 
 
 export default function TodoList(props){
-    const  {title,list}=props;
+    const  {title,list,addNewItem,filter,changeFilter}=props;
     const count  =list.length; 
+    const filteredList=applyFilter(list,filter)
     
         return(
             <div  className="todolist">
-                <Header title={title} />
+                <Header title={title} addNewItem={addNewItem}/>
 
                 <ul className="list-unstyled">
                 {list.map(item => <TodoItem key={item.id} data={item}/>)}
             </ul>
-            <Footer count={count}/>
+            <Footer count={{...count},filter, changeFilter}/>
+
             
             </div>
         )
+
+       
     }
+function  applyFilter(list,filter){
+
+    switch(filter){
+        case 'completed':
+            return list.filter(item => item.completed === true);
+
+        case 'active':
+            return list.filter(item => item.completed !== true);
+
+        default:
+            return list;
+    }
+}
