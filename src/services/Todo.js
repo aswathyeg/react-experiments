9466ca247da7d5e6b51c50f3960c0a720ac750eb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-10-29 17:08:15 +0530

    added data here

---
+++ src/services/Todo.js
@@ -0,0 +1,47 @@
+/**
+ * Get the list of todo items.
+ * @return {Array}
+ */
+ export function getAll() {
+    return [
+        {
+            id: 1,
+            text: 'Learn Javascript',
+            completed: false
+        }, {
+            id: 2,
+            text: 'Learn React',
+            completed: false
+        }, {
+            id: 3,
+            text: 'Build a React App',
+            completed: false
+        }
+    ]
+}
+
+/**
+ * A counter to generate a unique id for a todo item.
+ * Can remove this logic when the todo is created using backend/database logic.
+ * @type {Number}
+ */
+let todoCounter = 1;
+
+function getNextId() {
+    return getAll().length + todoCounter++;
+}
+
+/**
+ * Adds a new item on the list and returns the new updated list (immutable).
+ *
+ * @param {Array} list
+ * @param {Object} data
+ * @return {Array}
+ */
+export function addToList(list, data) {
+    let item = Object.assign({
+        id: getNextId()
+    }, data);
+
+    return list.concat([item]);
+}
