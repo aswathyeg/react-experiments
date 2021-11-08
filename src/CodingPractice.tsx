c15454c48d5beba1ef05cbaa7d82be801e70682d
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-08 20:06:06 +0530

    added styling

---
+++ src/CodingPractice.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './CodingPractice.css';
+export default function CodingPractice(){
+    return(
+        <div className="parentdiv">
+            <button className="render-btn">Click</button>
+        </div>
+    )
+}
