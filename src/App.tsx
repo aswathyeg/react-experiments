f484bc3c0f79c864b7542a1cb5a19f548ab0b1a4
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-08 19:55:13 +0530

    new component rendered

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PostExample from './api-example/PostExample';
 import ChartExample from './ChartExample';
+import Card from './components/Card/Card';
+import FormExample from './components/FormExample';
+import PersistentDrawerLeft from './PersistentDrawerLeft';
+import Table from './Table';
 function App() {
   return (
     <div>
-      <ChartExample />
+      <PostExample />
     </div>
   )
 }
