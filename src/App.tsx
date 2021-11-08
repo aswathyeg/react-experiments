7b8a6570049582fe72c7686e177467a5398dd850
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-08 22:44:51 +0530

    rendered new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostExample from './api-example/PostExample';
 import ChartExample from './ChartExample';
+import CodingPractice from './CodingPractice';
 import Card from './components/Card/Card';
 import FormExample from './components/FormExample';
 import PersistentDrawerLeft from './PersistentDrawerLeft';
@@ -8,7 +9,7 @@ import Table from './Table';
 function App() {
   return (
     <div>
-      <PostExample />
+      <CodingPractice />
     </div>
   )
 }
