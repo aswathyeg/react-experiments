20f7157ffe5555bd26f3e8cfae38a8136ff4c1a0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-08 22:58:06 +0530

    file changed as js

--- src/CodingPractice.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './CodingPractice.css';
-export default function CodingPractice(){
-    return(
-        <div className="parentdiv">
-            <button className="render-btn">Click</button>
-        </div>
-    )
-}
