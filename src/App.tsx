acb7aae5a5646bcd63494a5589f402d04b4028fa
aswathyeg <aswathy.eg.bijesh@gmail.com>
2021-11-10 20:15:55 +0530

    mounting rendered

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import ChartExample from './ChartExample';
 import CodingPractice from './CodingPractice';
 import Card from './components/Card/Card';
 import FormExample from './components/FormExample';
+import Building from './contextApI/Building';
+import ContextExample from './ContextExample';
+import Mounting from './Mounting';
 import PersistentDrawerLeft from './PersistentDrawerLeft';
 import Table from './Table';
 function App() {
   return (
     <div>
-      <CodingPractice />
+      <Mounting />
     </div>
   )
 }
