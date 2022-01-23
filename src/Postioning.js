08b063ccdf5c0330370495cf3be0dc053f1953c0
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-01-23 12:18:25 +0530

    added Grid

---
+++ src/Postioning.js
@@ -0,0 +1,11 @@
+import { Grid } from '@material-ui/core';
+import React from 'react';
+export default function Positioning(){
+    return(
+        <div>
+            <Grid>
+                
+            </Grid>
+        </div>
+    )
+}
