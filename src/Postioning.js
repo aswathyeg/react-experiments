5043d18f13fc4d08dbb7c4f781ebf688eb377252
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-01-23 13:08:41 +0530

    mui grid styling added

--- src/Postioning.js
+++ src/Postioning.js
@@ -1,11 +1,50 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid, TextField, Typography } from '@material-ui/core';
 import React from 'react';
-export default function Positioning(){
-    return(
-        <div>
-            <Grid container direction="column" alignItems="center" justify="center">
+export default function Positioning() {
+    return (
 
+        <Grid container
+            direction="column"
+            alignItems="center"
+            justify="center"
+            style={{ minHeight: "100vh" }}
+            spacing={5}
+        >
+            <Grid item>
+                <Typography varient="h5" color="primary" >
+                    Login Here
+                </Typography>
             </Grid>
-        </div>
+
+            <Grid item style={{ border: "0.2px solid gray" }} >
+                <Grid container
+                    direction="column"
+                    alignItems="center"
+                    justify="center"
+                    
+
+                >
+                    <TextField 
+                    variant="outlined" 
+                    label="Email" 
+                    fullWidth 
+                    style={{marginBottom:"2em"}}
+                    />
+
+
+                    <TextField 
+                    variant="outlined" 
+                    label="Password" 
+                    fullWidth 
+                    style={{marginBottom:"2em"}}
+                    />
+
+
+                    <Button size="large" variant="contained" color="primary">Login</Button>
+                </Grid>
+            </Grid>
+
+        </Grid>
+
     )
 }
