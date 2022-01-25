2de1d1b627e1838345a92a3a0c25a4a332039054
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-01-25 16:19:34 +0530

    updated

--- src/CodingChallenge/FetchDataExample.js
+++ src/CodingChallenge/FetchDataExample.js
@@ -3,37 +3,32 @@ import axios from "axios";
 
 export default function FetchDataExample(){
     const [data,setData]=useState([]);
-   const fetchdata=async()=>{
-
-        const {data}=await axios.get( 
-            "https://jsonplaceholder.typicode.com/posts/1/comments");
-            setData(data);
+    
+    const fetchData=async()=>{
+        const {data}=await axios.get( "https://jsonplaceholder.typicode.com/posts/1/comments");
+        setData(data);
 
     }
     useEffect(()=>{
-        fetchdata()
+        fetchData();
     },[]);
     return(
-    <div>
-<ui >
-{data.map((item)=>{
-    return(
         <div>
-    <li>
-Id:{item.id}
-</li>
-<li>
-Name:{item.name}
-</li>
-<li>
-Email:{item.email}
-</li>
-</div>
+            <ui>
+                {data.map((item)=>{
+                    return(
+                        <div>
+
+                <li>Id:{item.id}</li>
+                <li>Name:{item.name}</li>
+                <li>Email:{item.email}</li>
+                </div>
+                    )
+                })}
+                
+            </ui>
+        </div>
     )
-})}
-    
-</ui>
-    </div>
-    )
+   
 }
 
