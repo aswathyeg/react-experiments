7ac4125b903f5c30d9758fd12de702db2b6ebfa2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-10 00:36:40 +0530

    updated as class component

--- src/passingData/ChangeColor/ChildColor.js
+++ src/passingData/ChangeColor/ChildColor.js
@@ -1,18 +1,20 @@
+import React from 'react';
+export default class ChildColor extends React.Component {
 
-export default function ChildColor (props){
-    
-const handleClick = (e) =>{
-props.onColorChange(e.target.value);
-}
-  
-        return(
-            <div className="child-component"> 
-             <h4>Child Component</h4>
-            <div className="child-component button">
-               <button  value="Red" onClick={handleClick}>Red</button>
-               <button  value="Blue"onClick={handleClick}>Blue</button>
-               <button  value="White"onClick={handleClick}>Green</button>
-               </div>
+    handleClick = (e) => {
+        this.props.onColorChange(e.target.value);
+    }
+    render() {
+        return (
+            <div className="child-component">
+                <h4>Child Component</h4>
+                <div className="child-component button">
+                    <button value="Red" onClick={this.handleClick}>Red</button>
+                    <button value="Blue" onClick={this.handleClick}>Blue</button>
+                    <button value="White" onClick={this.handleClick}>White</button>
+                </div>
             </div>
         )
     }
+
+}
