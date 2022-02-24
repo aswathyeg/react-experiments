2489ad814802bb21ef313bffe9bf37224b04d300
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-02-25 00:21:08 +0530

    consuming multiple contexts

--- src/useContexExample/Navbar.js
+++ src/useContexExample/Navbar.js
@@ -1,22 +1,38 @@
+
+//acccessing context method 2 and consuming multiple contexts
 import React from 'react';
 import './index.css';
 import {ThemeContext} from './contexts/ThemeContext';
+import { AuthContext } from './contexts/AuthContext';
 class Navbar extends React.Component{
     static contextType=ThemeContext;//property which access this.value from ThemeContext
     render(){
-        console.log(this.context);
-        const{isLightTheme,light,dark}=this.context;
-        const theme=isLightTheme?light:dark;
-        return(
-          <nav style={{background:theme.ui,color:theme.syntax}}>
-                <h1>React Usecontext</h1>
-               <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                    </ul>
-                    </nav>
-        )
-    }
-}
+                return(
+            <AuthContext.Consumer >{(authContext)=> //wrapped themecontext with authcontext
+            (
+                <ThemeContext.Consumer>{(themeContext)=>{
+                    const {isAuthenticated,isToggle}=authContext;
+                    const{isLightTheme,light,dark}=themeContext;
+                   const theme=isLightTheme?light:dark;
+
+                
+                return( <nav style={{background:theme.ui,color:theme.syntax}}>
+                    <h1>React Usecontext</h1>
+                    <div onClick={isToggle}>
+                        {isAuthenticated? 'Logged In':'Logged Out'}
+                    </div>
+                   <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Contact</li>
+                        </ul>
+                        </nav>
+             
+            )
+        }}
+    </ThemeContext.Consumer>
+            )}
+            
+</AuthContext.Consumer>
+        );}}
 export default Navbar;
