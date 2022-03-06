66e510ddaf8bf28b388249b7e6a135b7376826dd
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-03-06 15:10:40 +0530

    added button

---
+++ src/Strings/UI/Button.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Button.module.css';
+export default function Button(props) {
+    <div className={classes.button}>
+        <button type={props.type||'submit'}
+        onClick={props.onClick}>
+{props.children}
+        </button>
+    </div>
+}
