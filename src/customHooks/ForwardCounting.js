c1caeff07ac32b698f82bf84193c1ed4c78d56d2
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-04-08 10:49:00 +0530

    updated

--- src/customHooks/ForwardCounting.js
+++ src/customHooks/ForwardCounting.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 const ForwardCounting = () =>{
+    const [counter,setCounter] = useState(0);
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            setCounter((prevState) =>{
+                prevState+1;
+            })     
+
+        },1000)
+        return ()=> clearInterval(interval);
+    },[])
     return(
         <div>
-            
+         <Card>{counter}</Card>   
         </div>
     )
 
