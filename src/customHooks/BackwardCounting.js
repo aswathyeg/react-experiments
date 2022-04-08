d5dd26cf044e05edc77d8714361aa8171e0db75f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-04-08 06:19:37 +0530

    customHooks example

---
+++ src/customHooks/BackwardCounting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const BackwardCounting = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+export default BackwardCounting;
---
+++ src/customHooks/ForwardCounting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const ForwardCounting = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
+export default ForwardCounting;
