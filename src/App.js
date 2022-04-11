7d9f55e65c99658ae6910e44eddfae72dfb1ca62
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-04-11 18:34:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,44 @@ import './App.css';
 import './useContextExample/index.css';
 import ForwardCounting from './customHooks/ForwardCounting';
 import BackwardCounting from './customHooks/BackwardCounting';
+import MovieList from './components/sendingHttp/MovieList';
 
 function App() {
-  return (
-    <div className="App">
+  const [movies,setMovies] = useState([]);
+  function fetchMoviesHandler(){
+    
+    fetch('https://swapi.dev/api/films/').
+    then((response) => {
+      return response.json();
 
-<ForwardCounting/>
-<BackwardCounting/>
+    })
+    .then ((data)=>{
+      const transformedMovies = data.results.map(movieData) => {
+
+        return{
+          id:movieData.episode_id,
+          title:movieData.title,
+          openingText:movieData.opening_crawl,
+          releaseDate:movieData.release_date,
+
+        };
+      });
+      setMovies(transformedMovies);
+    });
+  }
+  
+  return (
+   <React.Fragment>
+     <section>
+       <button onClick={fetchMoviesHandler}>
+         Fetch movies
+       </button>
+     </section>
+     <section>
+       <MovieList movies={movies}/>
+     </section>
      
-    </div>
+     </React.Fragment>
   );
   
 
