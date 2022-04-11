c3284b35511191afc6f10d797925cb2cb47e317c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-04-11 20:35:49 +0530

    added async

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import './App.css';
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map((movieData) => {
+  async function fetchMoviesHandler() {
+   const response = await fetch('https://swapi.dev/api/films/');
+      
+    const data = await response.json();
+     
+              const transformedMovies = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
             title: movieData.title,
@@ -20,8 +19,9 @@ function App() {
           };
         });
         setMovies(transformedMovies);
-      });
+      
   }
+ 
 
   return (
     <React.Fragment>
