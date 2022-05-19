b9db3f8278738718ef9b7d197a4711b1a9489787
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-05-19 17:15:42 +0530

    root folder renamed

--- src/Expences/ChildDate.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function  ChildDate(props){
-    const date=props.date.toLocaleString('en-US', { month: 'long' });
-    return(
-        <div>
-           <h2> {date}</h2>
-        </div>
-    )
-}
