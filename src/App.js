1c48d5147346a122695466011f81995c3bd89509
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-14 12:24:40 +0530

    added object

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import * as React from 'react';
+import React, { useState } from "react";
 import Ingredients from './satateManagement/components/ingredients/Ingredients';
-import Login from './Login';
+import DisplayNested from './DisplayNested';
 export const App = props => {
-  return <Login/>
+  const [nestedObjected, setNestedObject] = useState({
+    taxi: "a car licensed to transport passengers in return for payment of a fare",
+    food: {
+      sushi:
+        "a traditional Japanese dish of prepared rice accompanied by seafood and vegetables",
+      apple: {
+        Honeycrisp:
+          "an apple cultivar developed at the MAES Horticultural Research Center",
+        Fuji: "an apple cultivar developed by growers at Tohoku Research Station",
+      },
+    },
+  });
+
+  return <DisplayNested nestedObjected={nestedObjected}/>
 };
 
 export default App;
