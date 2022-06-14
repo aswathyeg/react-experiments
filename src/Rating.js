2ca8d6146d8dd6ace0e98f09d76767731d6c7081
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-14 12:26:04 +0530

    star rating example

---
+++ src/Rating.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Rating = ( ) =>{
+    const EMPTY_STAR =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1088px-Five-pointed_star.svg.png";
+  const FULL_STAR =
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png";
+    return(
+        <div>
+
+        </div>
+    )
+
+}
+export default Rating;
