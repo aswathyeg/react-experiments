994479016a7fbf4677f68c04edda7f551b7c37ce
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-17 21:14:48 +0530

    robot list app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React, { useState } from "react";
-import Ingredients from './satateManagement/components/ingredients/Ingredients';
-import Rating from './Rating';
-import Quiz from './Quiz';
-import CheckList from "./CheckList";
+import RobotList from "./RobotList";
 const App = () => {
-  return <CheckList/>
+  return <RobotList/>
 };
 
 export default App;
