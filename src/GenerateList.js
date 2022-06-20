252e15de3773e2cf41ec8829c1d06abb811fd6b7
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-20 14:06:48 +0530

    mapped to list

--- src/GenerateList.js
+++ src/GenerateList.js
@@ -3,9 +3,31 @@ import { useEffect, useState } from "react";
 
 const GenerateList = () => {
 
-  
+  const [activityList,setActivityList] =useState([]);
+   const genatateActivity = ( ) =>{
+    const 
+   }
+   
     return(
-        <div>
+        <div  style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          padding: 20,
+        }}>
+          <input
+           placeholder = "GenerateActivity"
+           type = {"button"}
+           onClick = {genatateActivity}
+           />
+           <ul>
+            {activityList.map((act)=>{
+              return(
+                <ExpandableList item = {act}/>
+              )
+            })}
+           </ul>
             
         </div>
     )
