391fab70d5e1cb1a8b2c1d3ec7082657c5c28428
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-21 14:48:49 +0530

    delete app

---
+++ src/DeleteApp.js
@@ -0,0 +1,12 @@
+const DeleteApp = () =>{
+    const handleDelete = () =>{
+        
+    }
+    return(
+        <div>
+            <h1>Delete App</h1>
+            <button onClick ={handleDelete}>Delete</button>
+        </div>
+    )
+}
+export default DeleteApp;
