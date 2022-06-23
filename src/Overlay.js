0c7800f24cfe4ebae39b442a07f115ca53b6a644
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-23 12:14:37 +0530

    model component

---
+++ src/Overlay.js
@@ -0,0 +1,10 @@
+const Overlay = (props) => {
+  return (
+    <div>
+      <h1>Are you sure?</h1>
+      <button>Cancel</button>
+      <button>Confirm</button>
+    </div>
+  );
+};
+export default Overlay;
