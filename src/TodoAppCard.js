80c4cde937c189c9b45699aad5b466afa68d7dd5
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-23 15:08:01 +0530

    added cancel eventhandler

--- src/TodoAppCard.js
+++ src/TodoAppCard.js
@@ -1,12 +1,26 @@
+import BackDrop from "./BackDrop";
+import { useState } from "react";
+import Overlay from "./Overlay";
 import classes from "./TodoApp.module.css";
+
 const TodoAppCard = (props) => {
-  const handleDelete = () => {};
+  const [state, setState] = useState(false);
+
+  const handleDelete = () => {
+    setState(true);
+  };
+  const handleCancel = () => {
+    setState(false);
+  };
+
   return (
     <div className={classes.card}>
       <h1>{props.text}</h1>
 
       <div className={classes.deleteButton}>
         <button onClick={handleDelete}>Delete</button>
+        {state == true && <Overlay />}
+        {state == true && <BackDrop onCancel={handleCancel} />}
       </div>
     </div>
   );
