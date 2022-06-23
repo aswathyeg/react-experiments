695e13bfa502426c6500325c0fa04f19e06b579a
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-06-23 15:07:26 +0530

    modal

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import classes from "./Overlay.module.css";
+
 const Overlay = (props) => {
+  const [confirmState, setConfirmState] = useState(false);
+  const handleConfirm = () => {
+    setConfirmState(true);
+  };
   return (
-    <div>
+    <div className={classes.Overlay}>
       <h1>Are you sure?</h1>
       <button>Cancel</button>
-      <button>Confirm</button>
+      <button onClick={handleConfirm}>Confirm</button>
     </div>
   );
 };
