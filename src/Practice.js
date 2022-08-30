ea86b2cacff6feef519d2360469952fc999f3520
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-08-30 16:49:54 +0530

    added useEffect

--- src/Practice.js
+++ src/Practice.js
@@ -1,12 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 const Practice = () => {
   const [state, setState] = useState("");
   const handleChange = (e) => {
     setState(e.target.value);
   };
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {}, 1000);
+    return () => clearTimeout(timeoutId);
+  }, [state]);
   return (
     <div>
-      <input placeholder="search here" type="text" onChange={handleChange} />
+      <input
+        placeholder="search here"
+        type="text"
+        value={state}
+        onChange={handleChange}
+      />
       <p>Search Result : {state}</p>
     </div>
   );
