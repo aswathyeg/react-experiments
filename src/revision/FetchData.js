a6facfe6781cbbc590a70b56b0f17f79ec47b05c
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-16 14:50:07 +0530

    customHook example

--- src/revision/FetchData.js
+++ src/revision/FetchData.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { useState, useEffect } from "react";
 const FetchData = () => {
-  useEffect(() => {}, []);
-  return <div>{`Below is the fetched data ${data}`}</div>;
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+  return (
+    <div>
+      {data &&
+        data.map((item) => {
+          return <p key={item.id}>{item.title}</p>;
+        })}
+    </div>
+  );
 };
 
 export default FetchData;
