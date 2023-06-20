532b27caf3a9b733e20e07a49e142002bcda8f7f
aswathyeg <aswathy.eg.bijesh@gmail.com>
2023-06-20 16:26:26 +0530

    updated

--- src/AddMe.js
+++ src/AddMe.js
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 const AddMe = () => {
   let [count, setCount] = useState(0);
   let onAdd = () => {
-    setCount((count = count + 1));
+    setCount(count + 1);
   };
 
   return (
